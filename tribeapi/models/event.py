bc912e9c83471c0905e2aec4e22467eb88d93d0d
Abbey Moss <abbeymossmusic@gmail.com>
2023-09-12 15:03:51 -0500

    TalkedthurfixtureswithSefora,before migrations

--- tribeapi/models/event.py
+++ tribeapi/models/event.py
@@ -4,9 +4,12 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=50)
     details = models.CharField(max_length=1000)
+    location = models.CharField(max_length=50)
     date = models.DateField()
     time = models.TimeField()
     host = models.ForeignKey("TribeUser", null=True, blank=True,
     on_delete=models.CASCADE, related_name="tribeUser_host")
     tags = models.ManyToManyField(
         "Tag", through="EventTag", related_name="event_tags")
+    
+# had to go back and add model property.. location!! remember to migrate that change!
